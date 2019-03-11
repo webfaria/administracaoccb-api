b44e98f2c2b4808bbde7cdc67eb6c6cddbdc4f5e
Cleber Faria <web_faria@yahoo.com.br>
2019-03-11 11:54:12 -0300

    Projeto criado

---
+++ src/main/java/com/farani/administracaoccbapi/AdministracaoccbApiApplication.java
@@ -0,0 +1,13 @@
+package com.farani.administracaoccbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracaoccbApiApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracaoccbApiApplication.class, args);
+	}
+
+}
