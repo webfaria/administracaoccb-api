44aed9c4a4879d11eb8258696e18e34166ac781f
Cleber Faria <web_faria@yahoo.com.br>
2019-03-14 18:11:23 -0300

    Consultando primeiro recurso com GET

---
+++ src/main/java/com/farani/administracaoccbapi/financeiro/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.farani.administracaoccbapi.financeiro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.farani.administracaoccbapi.financeiro.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
+
---
+++ src/main/java/com/farani/administracaoccbapi/financeiro/resource/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.farani.administracaoccbapi.financeiro.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.farani.administracaoccbapi.financeiro.model.Categoria;
+import com.farani.administracaoccbapi.financeiro.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+
+}
