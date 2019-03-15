a5ccb41d34b6554aedbc1d3c220d7e512cad6932
Cleber Faria <web_faria@yahoo.com.br>
2019-03-15 16:23:27 -0300

    Cadastrando nova categoria com POST

--- src/main/java/com/farani/administracaoccbapi/financeiro/resource/CategoriaResource.java
+++ src/main/java/com/farani/administracaoccbapi/financeiro/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.farani.administracaoccbapi.financeiro.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.farani.administracaoccbapi.financeiro.model.Categoria;
 import com.farani.administracaoccbapi.financeiro.repository.CategoriaRepository;
@@ -20,6 +29,22 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo);
 	}
 
 }
