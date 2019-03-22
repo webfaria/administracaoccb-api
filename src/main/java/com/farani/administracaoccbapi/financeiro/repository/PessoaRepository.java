b414d48b5f0cc8454f1c2924d5f036a72cdb9f63
Cleber Faria <web_faria@yahoo.com.br>
2019-03-22 18:06:51 -0300

    Criando estrutura completa de Pessoa - model, repository e resource

---
+++ src/main/java/com/farani/administracaoccbapi/financeiro/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.farani.administracaoccbapi.financeiro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.farani.administracaoccbapi.financeiro.model.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+}
