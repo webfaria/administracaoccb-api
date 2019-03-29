a8999f9a7e946d2f4fcfb729ae859d5798730664
Cleber Faria <web_faria@yahoo.com.br>
2019-03-29 18:51:51 -0300

    Cadastro de novo lançamento

--- src/main/java/com/farani/administracaoccbapi/financeiro/resource/LancamentoResource.java
+++ src/main/java/com/farani/administracaoccbapi/financeiro/resource/LancamentoResource.java
@@ -3,13 +3,21 @@ package com.farani.administracaoccbapi.financeiro.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.farani.administracaoccbapi.event.RecursoCriadoEvent;
 import com.farani.administracaoccbapi.financeiro.model.Lancamento;
 import com.farani.administracaoccbapi.financeiro.repository.LancamentoRepository;
 
@@ -20,9 +28,19 @@ public class LancamentoResource {
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Lancamento> listar() {
 		return lancamentoRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
 	
 	@GetMapping("/{codigo}")
